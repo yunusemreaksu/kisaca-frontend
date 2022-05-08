61b95f8821efb9cdc059ea4ea8d73a5df7a7d7d5
Yunus Emre Aksu <emre_aksu@live.com>
2022-05-08 17:37:30 +0300

    Added css module names for Omer

--- src/components/UI/ExpandButton.js
+++ src/components/UI/ExpandButton.js
@@ -4,13 +4,17 @@ import classes from "./ExpandButton.module.css";
 
 const ExpandButton = (props) => {
   const [expandText, setExpandText] = useState(true);
-  const [showButton, setShowButton] = useState(true)
+  const [showButton, setShowButton] = useState(true);
 
   const handleClick = () => {
-      setExpandText(!expandText)
-      setShowButton(!showButton)
-  }
-  return <button onClick={handleClick}>expand</button>;
+    setExpandText(!expandText);
+    setShowButton(!showButton);
+  };
+  return (
+    <button className={classes.button} onClick={handleClick}>
+      Expand
+    </button>
+  );
 };
 
 export default ExpandButton;
