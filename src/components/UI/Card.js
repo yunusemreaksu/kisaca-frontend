fe7273f5d037ebdf521085c093076916745f7b2f
Yunus Emre Aksu <emre_aksu@live.com>
2022-05-16 10:01:29 +0300

    Added detailed className to Card

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,11 @@ import React from "react";
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-  return <div className={classes.card}>{props.children}</div>;
+  return (
+    <div className={`${classes.card} ${classes.className}`}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Card;
